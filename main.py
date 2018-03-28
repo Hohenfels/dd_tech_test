48612698ce8107279cbdb72570f3c4307e51a64c
Felicien <frenaud@student.42.fr>
2018-03-28 12:18:56 +0200

    Ended but problems on CSV writing

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os, sys
 import csv
 import requests
 import math
-import pandas as pds
-from methods import *
+from methods import scrap_it, get_nb_articles, write_to_csv
 from bs4 import BeautifulSoup
 
 # First of all, make sure you have installed BeautifulSoup and reauests (see the README.txt), thank you ! Felicien RENAUD @ 42
@@ -12,7 +11,7 @@ from bs4 import BeautifulSoup
 def main():
     website = "https://search.rakuten.co.jp/search/mall/"
 
-    brand_to_search = input("What brand would you like to scrap on Rakuten.co.jp ? ")
+    brand_to_search = input("What brand would you like to scrap on Rakuten.co.jp ?\n")
     brand_formatted = brand_to_search.replace(" ", "+") # Replacing all the space with '+' to imitate rakuten.co.jp url style
 
     # The final url is divided in 3 parts :     
@@ -33,13 +32,23 @@ def main():
 
     # Retrieving number of articles with the method get_nb_articles and dividing it by 45 (number of article per page) the maximum is 150
 
-    nb_articles = math.trunc(get_nb_articles(soup.find("h1", {"class": "section"})) / 45) + 1
-    if nb_articles > 150:
-        nb_articles = 150
+    max_pages = math.trunc(get_nb_articles(soup.find("h1", {"class": "section"})) / 45) + 1
+    if max_pages > 150:
+        max_pages = 150
+
+    # Now asking the user how much pages he wants to scrap
+
+    nb_page = input("How much page ? (max for " + brand_to_search +" is " + str(max_pages) + ")\n")
+    nb_page = int(nb_page)
+    if nb_page > max_pages:
+        print("You entered a number of pages above the max page number\n\
+                Then, your default value will be " + str(max_pages))
+        nb_page = max_pages
 
     # Here is the big part, a big loop that's gonna retrieve every articles from N page(s)
     # I begin to find all the article with soup.find_all according to the right div
-    # then I do a for loop to get inside every div and begin the extraction of data
+    # I do a for loop to get inside every div and begin the extraction of data
+    # then I write my data to a .csv file with the name of the brand requested
 
     i = 1
     while True:
@@ -48,8 +57,9 @@ def main():
         soup = BeautifulSoup(request.content, "html.parser")
         articles = soup.find_all("div", {"class": "dui-card searchresultitem"})
         for article in articles:
-            scrap_it(article)
-        if i == 1:
+            table = scrap_it(article)
+            write_to_csv(table, brand_to_search)
+        if i == nb_page:
             break
         i += 1
 
--- methods.py
+++ methods.py
@@ -17,12 +17,61 @@ def get_desc(element):
 
 def get_price(element):
     data = element.find("span", {"class": "important"})
-    return data.text
+    return data.text.strip('円').replace(",", "") + " CPY"
 
+def get_picture(element):
+    data = element.find("img", {"class": "_verticallyaligned"})
+    return data.get('src')
+
+def get_seller_name(element):
+    data = element.find_all("a", {"target": "_top"})
+    return data[1].text
+
+def get_seller_url(element):
+    data = element.find_all("a", {"target": "_top"})
+    return data[1].get('href')
+
+def write_to_csv(table, brand):
+    import csv
+    import os.path
+
+    brand = brand.replace(" ", "_").lower()
+    file_path = "csv/" + brand + ".csv"
+
+    j = len(table)
+    i = 0
+    if os.path.isfile(file_path) is False:
+        open(file_path, 'a').close()
+    with open(file_path, "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        while i < j:
+            writer.writerow((
+                table["UR"] + '\n',
+                table["DC"] + '\n',
+                table["PR"] + '\n',
+                table["PU"] + '\n',
+                table["SN"] + '\n',
+                table["SU"] + '\n'
+                ))
+            i += 1
+        writer.writerow("====================")
+    file.close()
+        
 def scrap_it(element):
     url = get_url(element)
-    print(url)
-    desc = get_desc(element)
-    print(desc)
+    desc = get_desc(element) 
     price = get_price(element)
-    print(price)
+    picture = get_picture(element)
+    seller_name = get_seller_name(element)
+    seller_url = get_seller_url(element)
+
+    table = {
+        "UR": url,
+        "DC": desc,
+        "PR": price,
+        "PU": picture,
+        "SN": seller_name,
+        "SU": seller_url
+    }
+
+    return table
